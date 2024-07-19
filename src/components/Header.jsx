9b96cbc1ff239c330d843a00972c0deb519f3919
Wilson Bovea Cifuentes <wilsonboveacifuentes@MacBook-Air-de-Wilson.local>
2024-07-19 14:14:01 +0200

    modification header container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,120 +21,122 @@ const Header = ({
   const [switch1, setSwitch1] = useState(false);
 
   return (
-    <header
-      className={
-        offerId === 0
-          ? "container headerAndFilter"
-          : "header-not-filter container"
-      }
-    >
-      <div className="vinted">
-        <div className="logo-vinted">
-          <NavLink
-            to="/"
+    <header>
+      <div
+        className={
+          offerId === 0
+            ? "headerAndFilter container"
+            : "header-not-filter container"
+        }
+      >
+        <div className="vinted">
+          <div className="logo-vinted">
+            <NavLink
+              to="/"
+              onClick={() => {
+                setOfferId(0);
+              }}
+            >
+              <img
+                className="img-logo"
+                src="/Vinted_logo.png"
+                alt="logo vinted"
+              />
+            </NavLink>
+          </div>
+          <button
+            className="hidden-button modal"
             onClick={() => {
+              setDisplay(1);
+            }}
+          >
+            modal
+          </button>
+        </div>
+
+        <div className="barre-recherche">
+          <input
+            className="barre-inp"
+            type="text"
+            placeholder="🔍 Recherche des articles"
+            value={search}
+            onChange={(event) => {
+              return setSearch(event.target.value);
+            }}
+          />
+
+          <div className={offerId === 0 ? "filter-barre" : "hidden-button"}>
+            <div className="trier-par">
+              <p>Trier par prix :</p>
+              <div className="button-trier">
+                <button
+                  className={!switch1 ? "asc" : ""}
+                  onClick={() => {
+                    setSwitch1(false);
+                    setChecked("price-asc");
+                  }}
+                >
+                  ⬆️
+                </button>
+                <button
+                  className={switch1 ? "desc" : ""}
+                  onClick={() => {
+                    setSwitch1(true);
+                    setChecked("price-desc");
+                  }}
+                >
+                  ⬇️
+                </button>
+              </div>
+            </div>
+            <div className="slider-div">
+              <p>{!counter[1] ? "PrixMin" : counter[0] + "€"}</p>
+              <Slider counter={counter} setCounter={setCounter} />
+              <p>{!counter[1] ? "PrixMax" : counter[1] + "€"}</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            connected === false && !cookie
+              ? "buttons-connection hide-header-button"
+              : "hidden-button"
+          }
+        >
+          <NavLink to="/signup">
+            <button onClick={() => setOfferId(1)}>S'inscrire</button>
+          </NavLink>
+          <NavLink to="/login">
+            <button onClick={() => setOfferId(1)}>Se connecter</button>
+          </NavLink>
+        </div>
+        <div
+          className={
+            connected === true || cookie
+              ? "button-disconnect hide-header-button"
+              : "hidden-button"
+          }
+        >
+          <button
+            type="submit"
+            onClick={() => {
+              Cookies.remove("userToken");
+
+              setUserToken("");
+              setCookie("");
+              setConnected(false);
+
+              window.location.reload();
               setOfferId(0);
             }}
           >
-            <img
-              className="img-logo"
-              src="/Vinted_logo.png"
-              alt="logo vinted"
-            />
-          </NavLink>
+            Se déconnecter
+          </button>
         </div>
-        <button
-          className="hidden-button modal"
-          onClick={() => {
-            setDisplay(1);
-          }}
-        >
-          modal
+        <button className="button-sell hide-header-button">
+          Vends tes articles
         </button>
       </div>
-
-      <div className="barre-recherche">
-        <input
-          className="barre-inp"
-          type="text"
-          placeholder="🔍 Recherche des articles"
-          value={search}
-          onChange={(event) => {
-            return setSearch(event.target.value);
-          }}
-        />
-
-        <div className={offerId === 0 ? "filter-barre" : "hidden-button"}>
-          <div className="trier-par">
-            <p>Trier par prix :</p>
-            <div className="button-trier">
-              <button
-                className={!switch1 ? "asc" : ""}
-                onClick={() => {
-                  setSwitch1(false);
-                  setChecked("price-asc");
-                }}
-              >
-                ⬆️
-              </button>
-              <button
-                className={switch1 ? "desc" : ""}
-                onClick={() => {
-                  setSwitch1(true);
-                  setChecked("price-desc");
-                }}
-              >
-                ⬇️
-              </button>
-            </div>
-          </div>
-          <div className="slider-div">
-            <p>{!counter[1] ? "PrixMin" : counter[0] + "€"}</p>
-            <Slider counter={counter} setCounter={setCounter} />
-            <p>{!counter[1] ? "PrixMax" : counter[1] + "€"}</p>
-          </div>
-        </div>
-      </div>
-      <div
-        className={
-          connected === false && !cookie
-            ? "buttons-connection hide-header-button"
-            : "hidden-button"
-        }
-      >
-        <NavLink to="/signup">
-          <button onClick={() => setOfferId(1)}>S'inscrire</button>
-        </NavLink>
-        <NavLink to="/login">
-          <button onClick={() => setOfferId(1)}>Se connecter</button>
-        </NavLink>
-      </div>
-      <div
-        className={
-          connected === true || cookie
-            ? "button-disconnect hide-header-button"
-            : "hidden-button"
-        }
-      >
-        <button
-          type="submit"
-          onClick={() => {
-            Cookies.remove("userToken");
-
-            setUserToken("");
-            setCookie("");
-            setConnected(false);
-
-            window.location.reload();
-            setOfferId(0);
-          }}
-        >
-          Se déconnecter
-        </button>
-      </div>
-      <button className="button-sell hide-header-button">
-        Vends tes articles
-      </button>
     </header>
   );
 };
